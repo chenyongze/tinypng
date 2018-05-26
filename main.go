06547fdcc3b3e527d3e277ca87e66732155c6e00
chenyongze <sapphire.php@gmail.com>
2018-05-26 18:35:26 +0800

    up

---
+++ main.go
@@ -0,0 +1,170 @@
+package main
+
+import (
+	"encoding/json"
+	"flag"
+	"fmt"
+	"github.com/iikira/BaiduPCS-Go/pcsutil"
+	"github.com/iikira/BaiduPCS-Go/requester"
+	"github.com/iikira/BaiduPCS-Go/requester/multipartreader"
+	"github.com/iikira/BaiduPCS-Go/uploader"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+	"path/filepath"
+)
+
+const (
+	// Version 版本号
+	Version = "v1.0"
+)
+
+var (
+	isOverWrite  = false
+	printVersion = false
+)
+
+func init() {
+	flag.BoolVar(&isOverWrite, "w", false, "overwrite")
+	flag.BoolVar(&printVersion, "v", false, "print version")
+
+	flag.Parse()
+
+	if printVersion {
+		fmt.Printf("TinyPNG client, Version %s\n", Version)
+		fmt.Println("Copyright (c) 2017-2018, iikira/tinypng: https://github.com/iikira/tinypng")
+		os.Exit(0)
+	}
+	pcsutil.SetLogPrefix()
+}
+
+func main() {
+	if len(os.Args) <= 1 {
+		flag.Usage()
+		return
+	}
+
+	for k := range flag.Args() {
+	for_1:
+		for errTimes := 0; errTimes < 3; errTimes++ {
+			code := do(flag.Arg(k))
+
+			switch code {
+			case 0:
+				fallthrough
+			case 1: // 系统错误
+				break for_1
+			case 2: // 网络错误
+				fallthrough
+			case 3: // json 解析错误
+				continue
+			}
+		}
+	}
+}
+
+type UploadedData struct {
+	Input   InputData  `json:"input"`
+	Output  OutputData `json:"output"`
+	Error   string     `json:"error"`
+	Message string     `json:"message"`
+}
+
+type InputData struct {
+	Size int64  `json:"size"`
+	Type string `json:"type"`
+}
+
+type OutputData struct {
+	InputData
+	Width  uint    `json:"width"`
+	Height uint    `json:"height"`
+	Ratio  float64 `json:"ratio"`
+	URL    string  `json:"url"`
+}
+
+func do(filename string) (code int) {
+	log.Printf("[%s] 正在上传图片\n", filename)
+	file, err := os.OpenFile(filename, os.O_RDONLY, 0666)
+	if err != nil {
+		log.Println(err)
+		return 1
+	}
+
+	// 保留权限
+	mode := os.FileMode(0666)
+	info, err := file.Stat()
+	if err == nil {
+		mode = info.Mode()
+	}
+
+	uploader.DoUpload("https://tinypng.com/web/shrink", multipartreader.NewFileReadedLen64(file), nil, func(resp *http.Response, err error) {
+		file.Close()
+		fmt.Println()
+
+		if err != nil {
+			log.Println(err)
+			code = 2
+			return
+		}
+
+		imgJSON, err := ioutil.ReadAll(resp.Body)
+		if err != nil {
+			log.Println(err)
+			code = 1
+			return
+		}
+
+		resp.Body.Close()
+
+		data := new(UploadedData)
+
+		err = json.Unmarshal(imgJSON, data)
+		if err != nil {
+			log.Println(err)
+			code = 3
+			return
+		}
+
+		if data.Error != "" {
+			log.Printf("[%s] Error, %s: %s\n", filename, data.Error, data.Message)
+			code = 1
+			return
+		}
+
+		log.Printf("[%s] 上传图片成功, 正在下载压缩后的图片...\n", filename)
+
+		img, err := requester.Fetch("GET", data.Output.URL, nil, nil)
+		if err != nil {
+			log.Println(err)
+			code = 2
+			return
+		}
+
+		imgLen := int64(len(img))
+		if imgLen != data.Output.Size {
+			log.Printf("[%s] 图片下载失败, 文件大小不一致, 已下载: %d, 远程: %d\n", filename, imgLen, data.Output.Size)
+			code = 2
+			return
+		}
+
+		var newName string
+		if isOverWrite {
+			newName = filename
+		} else {
+			newName = filepath.Dir(filename) + string(os.PathSeparator) + "tinified-" + filepath.Base(filename)
+		}
+
+		err = ioutil.WriteFile(newName, img, mode)
+		if err != nil {
+			log.Println(err)
+			code = 1
+			return
+		}
+
+		log.Printf("[%s] 图片保存成功, 保存位置: %s, 图片类型: %s, 图片宽度: %d, 图片高度: %d, 原始图片大小: %s, 压缩后图片大小: %s, 压缩比率: %f%%\n", filename, newName, data.Output.Type, data.Output.Width, data.Output.Height, pcsutil.ConvertFileSize(data.Input.Size), pcsutil.ConvertFileSize(data.Output.Size), data.Output.Ratio*100)
+	})
+
+	return code
+}
---
+++ vendor/github.com/iikira/BaiduPCS-Go/pcsutil/file.go
@@ -0,0 +1,45 @@
+package pcsutil
+
+import (
+	"github.com/iikira/osext"
+	"os"
+	"path/filepath"
+	"strings"
+)
+
+// ExecutablePath 获取程序所在目录
+func ExecutablePath() string {
+	folderPath, err := osext.ExecutableFolder()
+	if err != nil {
+		folderPath, err = filepath.Abs(filepath.Dir(os.Args[0]))
+		if err != nil {
+			folderPath = filepath.Dir(os.Args[0])
+		}
+	}
+	return folderPath
+}
+
+// ExecutablePathJoin 返回程序所在目录的子目录
+func ExecutablePathJoin(subPath string) string {
+	return filepath.Join(ExecutablePath(), subPath)
+}
+
+// WalkDir 获取指定目录及所有子目录下的所有文件，可以匹配后缀过滤。
+func WalkDir(dirPth, suffix string) (files []string, err error) {
+	files = make([]string, 0, 30)
+	suffix = strings.ToUpper(suffix) //忽略后缀匹配的大小写
+
+	err = filepath.Walk(dirPth, func(filename string, fi os.FileInfo, err error) error { //遍历目录
+		if err != nil {
+			return err
+		}
+		if fi.IsDir() { // 忽略目录
+			return nil
+		}
+		if strings.HasSuffix(strings.ToUpper(fi.Name()), suffix) {
+			files = append(files, filename)
+		}
+		return nil
+	})
+	return files, err
+}
